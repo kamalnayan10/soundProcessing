d9f2171517e3def80c1995338af5eae6b4daf49a
PickleRick1010 <kamalnayan04@gmail.com>
2023-02-09 23:16:16 +0530

    preprocessed music data for genre classification

--- preprocess.py
+++ preprocess.py
@@ -1,54 +1,66 @@
-import librosa , librosa.display
-import matplotlib.pyplot as plt
-import numpy as np
+import os
+import librosa
+import math
+import json
 
-FILE = "blues.00000.wav"
+DATASET_PATH = "D:\PROGRAMMING\PYTHON\genres_original"
+JSON_PATH = "data.json"
 
-# WAVEFORM
+SAMPLE_RATE = 22050
 
-signal , sr =  librosa.load(FILE , sr = 22050)
+DURATION = 30 #measured in seconds
 
-# librosa.display.waveshow(signal , sr = sr)
+SAMPLES_PER_TRACK = SAMPLE_RATE * DURATION
 
-# plt.xlabel("Time")
-# plt.ylabel("Amplitude")
-# plt.show()
 
-# Perofrming a FFT(Fast Fourier Transform)
+def save_mfcc(dataset_path, json_path, n_mfcc = 13 , n_fft = 2048 , hop_length = 512 , num_segments = 5):
+    # Build Dictionary to store data
+    data = {
+        "mapping":[],
+        "mfcc":[],
+        "labels":[]
+    }
 
-fft = np.fft.fft(signal)
+    num_samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
+    expected_num_mfcc_vectors_per_segment = math.ceil(num_samples_per_segment/hop_length)
 
-magnitude = np.abs(fft)
-frequency = np.linspace(0,sr,len(magnitude))
+    # Loop thorugh all the genres
 
-left_frequency = frequency[:int(len(frequency)/2)]
-left_magnitude = magnitude[:int(len(magnitude)/2)]
+    for i , (dirpath , dirnames ,filenames) in enumerate(os.walk(dataset_path)):
+        
+        # Ensure that we're not at the root level
+        if dirpath is not dataset_path:
+            dirpath_components = dirpath.split("/") #genres/blues -> ['genres , 'blues']
+            semantic_label = dirpath_components[-1]
+            data["mapping"].append(semantic_label)
 
-# plt.plot(left_frequency,left_magnitude)
-# plt.xlabel("Frequency")
-# plt.ylabel("Magnitude")
-# plt.show()
+            print(f"\n Processing {semantic_label}")
 
-# Creating STFT(Short Time Fourier Transform)
-n_fft = 2048
-hop_len = 512
-stft = librosa.core.stft(signal , n_fft = n_fft , hop_length = hop_len)
+            # Process files for a specific genre
+            for f in filenames:
+                # load audio file
+                file_path = os.path.join(dirpath , f)
+                signal , sr = librosa.load(file_path , sr = SAMPLE_RATE)
 
-spectrogram = np.abs(stft)
+                #Process segments extracting mfcc and storing data
+                for s in range(num_segments):
+                    start_sample = num_samples_per_segment*s
+                    finish_sample = start_sample + num_samples_per_segment
 
-log_spectrogram = librosa.amplitude_to_db(spectrogram)
+                # Store mfcc for segment if it has expected length
+                    mfcc = librosa.feature.mfcc(signal[start_sample:finish_sample] , sr = sr , n_mfcc = n_mfcc , n_fft = n_fft , hop_length = hop_length)
 
-# librosa.display.specshow(log_spectrogram , sr = sr , hop_length=hop_len)
-# plt.xlabel("Time")
-# plt.ylabel("Frequency")
-# plt.colorbar()
-# plt.show()
+                    mfcc = mfcc.T
 
-# Calculating MFCC(Mel-Frequency Cepstral Coefficients)
+                    if len(mfcc) == expected_num_mfcc_vectors_per_segment:
+                        data["mfcc"].append(mfcc.tolist())
+                        data["labels"].append(i-1)
+                        print(f"{file_path} , {s}")
 
-MFCCs = librosa.feature.mfcc(signal ,n_fft = n_fft , hop_length = hop_len , n_mfcc = 13)
-librosa.display.specshow(MFCCs , sr = sr , hop_length=hop_len)
-plt.xlabel("Time")
-plt.ylabel("MFCC")
-plt.colorbar()
-plt.show()
+    with open(JSON_PATH , "w") as fp:
+        json.dump(data , fp)
+
+if __name__ == "__main__":
+    
+    save_mfcc(DATASET_PATH , JSON_PATH , num_segments = 10)
+    
